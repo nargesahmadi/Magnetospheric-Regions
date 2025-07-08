a1717e1f7fc319469c25c4fd11abdf946a03de61
Narges Ahmadi <Narges.Ahmadi@lasp.colorado.edu>
2025-07-08 14:12:35 -0600

    Starting region model

---
+++ utilities.py
@@ -0,0 +1,120 @@
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import roc_curve, auc
+from itertools import cycle
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def roc_plot_rf(y_pred, y_true, association=False):
+    # Plot linewidth.
+    lw = 2.0
+
+    classes_list = np.unique(y_true)
+    n_classes = len(classes_list)
+    
+    print('n_classes', n_classes)
+    
+    y_true_roc = label_binarize(y_true, classes=classes_list)
+    y_score_proba = label_binarize(y_pred, classes=classes_list)
+    
+    # Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_true_roc[:, i], y_score_proba[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_true_roc.ravel(), y_score_proba.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    # Compute macro-average ROC curve and ROC area
+
+    # First aggregate all false positive rates
+    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+    # Then interpolate all ROC curves at this points
+    mean_tpr = np.zeros_like(all_fpr)
+    for i in range(n_classes):
+        mean_tpr += np.interp(all_fpr, fpr[i], tpr[i])
+
+    # Finally average it and compute AUC
+    mean_tpr /= n_classes
+
+    fpr["macro"] = all_fpr
+    tpr["macro"] = mean_tpr
+    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+    # Plot all ROC curves
+    fig = plt.figure(1)
+
+    full_list = ['aqua', 'darkorange', 'cornflowerblue', 'limegreen', 'firebrick', 'orchid']
+    if len(full_list) > n_classes:
+        colors = cycle(full_list[:n_classes])
+    else:
+        colors = cycle(full_list)
+    for i, color in zip(range(n_classes), colors):
+        plt.plot(fpr[i], tpr[i], lw=lw,
+                 label='{0} (AUC = {1:0.2f})'
+                 ''.format(association[i], roc_auc[i]))
+
+    plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.02])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    #plt.title('Some extension of Receiver operating characteristic to multi-class')
+    plt.legend(loc="lower right", prop={"size":9})
+    fig.savefig('AUC.png', dpi=fig.dpi)
+
+    print('AUC macro average :' + str(roc_auc["macro"]))
+    print('AUC micro average :' + str(roc_auc["micro"]))
+
+
+    return fig
+
+def plot_loss_curves(results):
+    """Plots training curves of a results dictionary.
+
+    Args:
+        results (dict): dictionary containing list of values, e.g.
+            {"train_loss": [...],
+             "train_acc": [...],
+             "test_loss": [...],
+             "test_acc": [...]}
+    """
+    
+    # Get the loss values of the results dictionary (training and test)
+    loss = results['train_loss']
+    test_loss = results['test_loss']
+
+    # Get the accuracy values of the results dictionary (training and test)
+    accuracy = results['train_acc']
+    test_accuracy = results['test_acc']
+
+    # Figure out how many epochs there were
+    epochs = range(len(results['train_loss']))
+
+    # Setup a plot 
+    plt.figure(figsize=(15, 7))
+
+    # Plot loss
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, loss, label='train_loss')
+    plt.plot(epochs, test_loss, label='test_loss')
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+
+    # Plot accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, accuracy, label='train_accuracy')
+    plt.plot(epochs, test_accuracy, label='test_accuracy')
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend();
+    plt.savefig('LossAccuracy.png')
+    plt.show()
+    
+
